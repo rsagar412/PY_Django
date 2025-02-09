deefc623ac5a00ee4d0f6c58c26b8126fb592f68
Sagar Rathod <rathod.sagar23@yahoo.com>
2025-02-09 12:32:46 +0530

    templates html/static demo

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return HttpResponse("This is the homepage of the Django WebApp")
+
+    context = {
+        'variable': "This is a defined variable demo from views page using the context dict. It is declared using {{variable}} in the index.html file"
+    }
+    # return HttpResponse("This is the homepage of the Django WebApp")
+    return render(request, 'index.html', context)
 
 def about(request):
     return HttpResponse("this is the about page")
