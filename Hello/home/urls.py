c0913a484d526b06292fa9c7c349faad9d2b2755
Sagar Rathod <rathod.sagar23@yahoo.com>
2025-02-09 11:04:21 +0530

    url dispatcher (redirecting the urls)

--- Hello/home/urls.py
+++ Hello/home/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(home.urls))
+    path("", views.index, name = "homepage"),
+    path("about", views.about, name = "About"),
+    path("services", views.services, name ="Services"),
+    path("contact", views.contact, name ="contact")
 ]
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse("This is the homepage of the Django WebApp")
+
+def about(request):
+    return HttpResponse("this is the about page")
+
+def services(request):
+    return HttpResponse("this is the service page")
+
+def contact(request):
+    return HttpResponse("this is the contact page")
 # Create your views here.
